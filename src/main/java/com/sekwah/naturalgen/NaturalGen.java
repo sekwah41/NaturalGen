5544295546151bfd7ad56ebdf541f1c4b254f1cc
Sekwah <contact@sekwah.com>
2022-03-23 03:04:54 +0000

    fix: placing blocks to set is_natural to false

--- src/main/java/com/sekwah/naturalgen/NaturalGen.java
+++ src/main/java/com/sekwah/naturalgen/NaturalGen.java
@@ -1,21 +1,18 @@
 package com.sekwah.naturalgen;
 
+import com.sekwah.naturalgen.block.BlocksToInject;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.network.NetworkRegistry;
-import net.minecraftforge.network.simple.SimpleChannel;
 
 @Mod(NaturalGen.MOD_ID)
 public class NaturalGen {
     public static final String MOD_ID = "naturalgen";
 
-    public static final String PROTOCOL_VERSION = "1";
+    public static final String PROTOCOL_VERSION = BlocksToInject.addNaturalTag.toString();
 
     public NaturalGen() {
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        SimpleChannel SYNC_CHANNEL = NetworkRegistry.ChannelBuilder
+        NetworkRegistry.ChannelBuilder
                 .named(new ResourceLocation(NaturalGen.MOD_ID, "server_check"))
                 .networkProtocolVersion(() -> PROTOCOL_VERSION)
                 .clientAcceptedVersions(PROTOCOL_VERSION::equals)
